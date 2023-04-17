bf76e590888f395f1c3fa66fa6bfa70f3ac826a3
JT514569 <514569@chrome.escg.ac.uk>
2023-04-17 15:15:11 +0100

    Player animations being added.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,7 +15,14 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed,body.velocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed,body.velocity.y);
+
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector2.one;
+
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
 
         if (Input.GetKey(KeyCode.Space))
         {
