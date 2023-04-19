2a4a15f4f92b18dd91194f91b3e97e434aded9b2
JT514569 <514569@chrome.escg.ac.uk>
2023-04-19 11:08:47 +0100

    Fixed up coding just like in tutorial 3 (Box Collider).

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,15 +6,17 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float jump;
+    [SerializeField] private LayerMask groundLayer;
     private Rigidbody2D body;
     private Animator anim;
-    private bool Grounded;
+    private BoxCollider2D boxCollider;
 
     private void Awake()
     {
         //Grab refs for rigidbody and animator
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
@@ -29,29 +31,30 @@ public class PlayerMovement : MonoBehaviour
         else if (horizontalInput < -0.01f)
             transform.localScale = new Vector3(-1, 1, 1);
 
-        if (Input.GetKey(KeyCode.Space) && Grounded)
+        if (Input.GetKey(KeyCode.Space) && isGrounded())
         {
             Jump();
         }
 
         //Set animator stuff
         anim.SetBool("Run", horizontalInput != 0);
-        anim.SetBool("Grounded", Grounded);
+        anim.SetBool("Grounded", isGrounded());
     }
 
     private void Jump()
     {
         body.velocity = new Vector2(body.velocity.x, jump);
         anim.SetTrigger("Jump");
-        Grounded = false;
     }
 
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Ground")
-        {
-            Grounded = true;
-        }    
+    }
+    
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(BoxCollider.bounds.center, BoxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return false;
     }
 }
