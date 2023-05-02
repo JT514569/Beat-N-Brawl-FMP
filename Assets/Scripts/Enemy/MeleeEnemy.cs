431063022a9e1855fea0197c1312ad7a1c9097eb
514569@chrome.escg.ac.uk <514569@chrome.escg.ac.uk>
2023-05-02 15:28:57 +0100

    Enemy attack animation fixed. Just needs damaging hitbox.

--- Assets/Scripts/Enemy/MeleeEnemy.cs
+++ Assets/Scripts/Enemy/MeleeEnemy.cs
@@ -31,7 +31,7 @@ public class MeleeEnemy : MonoBehaviour
             if (cooldownTimer >= attackCooldown)
             {
                 cooldownTimer = 0;
-                anim.SetTrigger("Attack Prep");
+                anim.SetTrigger("punch");
             }
         }
 
@@ -41,7 +41,7 @@ public class MeleeEnemy : MonoBehaviour
     {
         RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance, new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z), 0, Vector2.left, 0, playerLayer);
 
-        if(hit.collider != null)
+        if (hit.collider != null)
         {
             playerHealth = hit.transform.GetComponent<Health>();
         }
